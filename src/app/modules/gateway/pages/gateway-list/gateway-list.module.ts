5aa51da8afe5cc6d5a5af34caa21bdf6579952ca
lunox94 <gmrlunox94@gmail.com>
2022-03-26 13:44:14 -0400

    chore: refactor gateway module structure

--- src/app/modules/gateway/pages/gateway-list/gateway-list.module.ts
+++ src/app/modules/gateway/pages/gateway-list/gateway-list.module.ts
@@ -5,7 +5,7 @@ import { SharedModule } from 'src/app/shared/shared.module';
 import { NzDividerModule } from 'ng-zorro-antd/divider';
 import { NzButtonModule } from 'ng-zorro-antd/button';
 import { NzIconModule } from 'ng-zorro-antd/icon';
-import { GatewayComponentsModule } from '../../components/gateway-components/gateway-components.module';
+import { GatewayComponentsModule } from '../../components/gateway-components.module';
 
 const gatewayListRoutes: Route[] = [
     {
