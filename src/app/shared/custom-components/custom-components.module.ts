a70ca43eaf56655e3934a4eebf25883f94d8b4b9
lunox94 <gmrlunox94@gmail.com>
2022-03-27 16:36:35 -0400

    chore: remove old drawer structure

--- src/app/shared/custom-components/custom-components.module.ts
+++ src/app/shared/custom-components/custom-components.module.ts
@@ -1,7 +1,5 @@
 import { NgModule } from '@angular/core';
-import { DrawerComponent } from './drawer/drawer.component';
 import { SharedModule } from '../shared.module';
-import { NzDrawerModule } from 'ng-zorro-antd/drawer';
 import { GatewayFormComponent } from './gateway-form/gateway-form.component';
 import { NzButtonModule } from 'ng-zorro-antd/button';
 import { NzDividerModule } from 'ng-zorro-antd/divider';
@@ -9,17 +7,15 @@ import { NzInputModule } from 'ng-zorro-antd/input';
 import { NzFormModule } from 'ng-zorro-antd/form';
 
 @NgModule({
-    declarations: [DrawerComponent, GatewayFormComponent],
+    declarations: [GatewayFormComponent],
     imports: [
         SharedModule,
 
         // NG-ZORRO imports
-        NzDrawerModule,
         NzButtonModule,
         NzDividerModule,
         NzInputModule,
         NzFormModule,
     ],
-    exports: [DrawerComponent],
 })
 export class CustomComponentsModule {}
