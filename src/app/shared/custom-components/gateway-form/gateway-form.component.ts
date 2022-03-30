7e05576ef56a4b57f0d52dbcc4653334ccf164c6
lunox94 <gmrlunox94@gmail.com>
2022-03-30 18:11:16 -0400

    chore: refactor gateway form

--- src/app/shared/custom-components/gateway-form/gateway-form.component.ts
+++ src/app/shared/custom-components/gateway-form/gateway-form.component.ts
@@ -1,8 +1,15 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { NzDrawerRef } from 'ng-zorro-antd/drawer';
-import { Observable, Subject, timer } from 'rxjs';
-import { take } from 'rxjs/operators';
+import { from, Observable, Subject } from 'rxjs';
+import {
+    mapTo,
+    mergeAll,
+    shareReplay,
+    switchMap,
+    take,
+    tap,
+} from 'rxjs/operators';
 import { GatewayApiService } from 'src/app/core/api/gateway/gateway-api.service';
 import {
     Gateway,
@@ -27,27 +34,57 @@ export class GatewayFormComponent implements OnInit {
     /** Form with the fields to create or edit a gateway. */
     form!: FormGroup;
 
-    /** Controller to emit the loading state values. */
-    private _loadingController: Subject<boolean>;
-
     /** Whether or not the component is performing an API call. */
-    loading$: Observable<boolean>;
+    loading$!: Observable<boolean>;
+
+    private readonly _createGatewayRequest = new Subject<GatewayToCreate>();
+    private readonly _updateGatewayRequest = new Subject<{
+        uid: string;
+        gatewayToUpdate: GatewayToUpdate;
+    }>();
 
     constructor(
         private _drawerRef: NzDrawerRef<GatewayFormComponent, boolean>,
         private _fb: FormBuilder,
         private _gatewayApiService: GatewayApiService
-    ) {
-        // set up state management for this component
-        this._loadingController = new Subject();
-        this.loading$ = this._loadingController.asObservable();
-    }
+    ) {}
 
     ngOnInit(): void {
+        // create the form
         this.form = this._fb.group({
             name: [this.gateway?.name, [Validators.required]],
             ipv4: [this.gateway?.ipv4, [Validators.required, ipv4()]],
         });
+
+        // emits when a request to create a gateway is triggered
+        const afterGatewayCreate$ = this._createGatewayRequest.pipe(
+            switchMap(this._createGateway),
+            tap((_) => this._drawerRef.close(true))
+        );
+
+        // emits when a request to update a gateway is triggered
+        const afterGatewayUpdate$ = this._updateGatewayRequest.pipe(
+            switchMap(this._updateGateway),
+            tap((_) => this._drawerRef.close(true))
+        );
+
+        // indicates that a request to create or update a gateway has started
+        const loadStart$ = from([
+            this._createGatewayRequest,
+            this._updateGatewayRequest,
+        ]).pipe(mergeAll(), mapTo(true));
+
+        // indicates that a request to create or update a gateway has ended
+        const loadEnd$ = from([afterGatewayCreate$, afterGatewayUpdate$]).pipe(
+            mergeAll(),
+            mapTo(false)
+        );
+
+        // indicates whether a gateway is being created / updated
+        this.loading$ = from([loadStart$, loadEnd$]).pipe(
+            mergeAll(),
+            shareReplay(1)
+        );
     }
 
     /** Closes the drawer. */
@@ -55,31 +92,22 @@ export class GatewayFormComponent implements OnInit {
         this._drawerRef.close(false);
     }
 
-    /** Called when the submit but is clicked. */
+    /** Called when the submit button is clicked. */
     onSubmit(): void {
         if (this.form.valid) {
-            // submit data
-            this._loadingController.next(true);
-
             // if there is no gateway then this form was open to create a new
-            // gateway.
+            // gateway
             if (!this.gateway) {
                 const gatewayToCreate: GatewayToCreate = this.form.value;
-                this._gatewayApiService
-                    .post(gatewayToCreate)
-                    .pipe(take(1))
-                    .subscribe((_) => {
-                        this._drawerRef.close(true);
-                    });
-                // else then this form was open to edit a gateway.
+                // request to create a gateway
+                this._createGatewayRequest.next(gatewayToCreate);
             } else {
+                // else then this form was open to edit a gateway
                 const gatewayToUpdate: GatewayToUpdate = this.form.value;
-                this._gatewayApiService
-                    .put(this.gateway!.uid, gatewayToUpdate)
-                    .pipe(take(1))
-                    .subscribe(() => {
-                        this._drawerRef.close(true);
-                    });
+                this._updateGatewayRequest.next({
+                    uid: this.gateway!.uid,
+                    gatewayToUpdate,
+                });
             }
         } else {
             Object.values(this.form!.controls).forEach((control) => {
@@ -90,4 +118,15 @@ export class GatewayFormComponent implements OnInit {
             });
         }
     }
+
+    private readonly _createGateway = (gatewayToCreate: GatewayToCreate) =>
+        this._gatewayApiService.post(gatewayToCreate);
+
+    private readonly _updateGateway = ({
+        uid,
+        gatewayToUpdate,
+    }: {
+        uid: string;
+        gatewayToUpdate: GatewayToUpdate;
+    }) => this._gatewayApiService.put(uid, gatewayToUpdate);
 }
