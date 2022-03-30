0295a242767654ab8af32bf757624d77ad2f9de6
lunox94 <gmrlunox94@gmail.com>
2022-03-30 01:39:53 -0400

    chore: remove unused module

--- src/app/modules/gateway/pages/gateway-details/gateway-details.component.ts
+++ src/app/modules/gateway/pages/gateway-details/gateway-details.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
-import { combineLatest, merge, Observable, Subject } from 'rxjs';
+import { merge, Observable, Subject } from 'rxjs';
 import {
     filter,
     map,
