a7da7690a81bd419e4cbaa6224daf656912e3d0d
lunox94 <gmrlunox94@gmail.com>
2022-03-31 11:40:17 -0400

    chore: improve exception handling

---
+++ src/app/core/errors/app-errors.ts
@@ -0,0 +1,5 @@
+export class AppError extends Error {}
+
+export class BadRequestError extends AppError {}
+
+export class NotFoundError extends AppError {}
--- src/app/core/interceptors/global-error-handling.interceptor.ts
+++ src/app/core/interceptors/global-error-handling.interceptor.ts
@@ -5,6 +5,7 @@ import {
     HttpEvent,
     HttpResponse,
     HttpErrorResponse,
+    HttpStatusCode,
 } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Router } from '@angular/router';
@@ -14,6 +15,7 @@ import {
     CONNECTION_REFUSED_TOKEN,
     DETAILS_TOKEN,
 } from 'src/app/modules/status-pages/pages/server-error/server-error.component';
+import { AppError, BadRequestError, NotFoundError } from '../errors/app-errors';
 import { GlobalDrawerService } from '../services/global-drawer.service';
 
 @Injectable()
@@ -30,10 +32,18 @@ export class GlobalErrorHandlingInterceptor implements HttpInterceptor {
         return next.handle(req).pipe(
             catchError((error) => {
                 // just doing a very simple error handling
+                // skipping error 422 handling
                 if (error instanceof HttpErrorResponse) {
-                    if (error.status == 404) {
+                    if (error.status == HttpStatusCode.NotFound) {
                         this._drawerService.drawerRef?.close();
                         this._router.navigate(['status', 'not-found']);
+                        return throwError(
+                            new NotFoundError(error.error.message)
+                        );
+                    } else if (error.status == HttpStatusCode.BadRequest) {
+                        return throwError(
+                            new BadRequestError(error.error.message)
+                        );
                     } else if (error.status == 0) {
                         this._drawerService.drawerRef?.close();
                         const queryParams: any = {};
@@ -41,9 +51,15 @@ export class GlobalErrorHandlingInterceptor implements HttpInterceptor {
                         this._router.navigate(['status', 'server-error'], {
                             queryParams,
                         });
+                        return throwError(new AppError(error.error.message));
                     } else {
                         this._drawerService.drawerRef?.close();
                         this._router.navigate(['status', 'server-error']);
+                        const message = error.error.message;
+                        const messageProcessed = Array.isArray(message)
+                            ? message.join()
+                            : message;
+                        return throwError(new AppError(messageProcessed));
                     }
                 }
                 return throwError(error);
