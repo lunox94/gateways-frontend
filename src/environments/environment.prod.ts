afcbd035eb5f02539b1ec4d9d994438824d399bb
lunox94 <gmrlunox94@gmail.com>
2022-04-02 00:39:04 -0400

    chore: clean up

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,6 @@
 export const environment = {
-  production: true
+    production: true,
+
+    // In real application this should point to the production api.
+    apiUrl: 'http://localhost:3000/api/v1',
 };
